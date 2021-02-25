17b88c940463ab686b95ea584f1bf1a3df16ba93
dxt <doxtop@synrc.com>
2021-02-25 13:57:26 +0200

    elixir configuration

---
+++ mix.exs
@@ -0,0 +1,59 @@
+defmodule Emqx.MixProject do
+ use Mix.Project
+
+ def project do
+    [
+      app: :emqx,
+      version: "4.2.0",
+      description: "EMQ X Broker",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      erlc_paths: ["src"],
+      elixirc_options: [warnings_as_errors: true],
+      deps: deps()
+    ]
+ end
+
+ def application do
+    [
+      mod: {Emqx.App, []},
+      extra_applications: [:logger, :os_mon],
+      applications: [
+        :kernel,
+        :stdlib,
+        :inets,
+        :mnesia,
+        :asn1,
+        :crypto,
+        :gproc,
+        :public_key,
+        :ssl,
+        :ranch,
+        :esockd,
+        :jsone,
+        :gen_rpc,
+        :ssl_verify_fun,
+        :cowlib,
+        :cowboy,
+        :replayq,
+        :ekka,
+        :minirest
+      ]
+    ]
+ end
+
+ def deps do
+  [
+    {:gproc,    github: "voxoz/gproc"},
+    {:ekka,     github: "xio/ekka", ref: "v0.7"},
+    {:esockd,   github: "emqx/esockd", ref: "v5.7.3"},
+    {:gen_rpc,  github: "xio/gen_rpc"},
+    {:replayq,  github: "xio/replayq"},
+    {:minirest, github: "xio/minirest", ref: "emqx42"},
+    {:jsone,    "~> 1.5.5", override: true},
+    {:cowlib,   "~> 2.9.1", override: true},
+    {:cowboy,   "~> 2.8.0", override: true},
+    {:ranch,    "~> 1.7.1", override: true}
+  ]
+ end
+end

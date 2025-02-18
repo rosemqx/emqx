299f10531f81a6be1c1344c88bf4928c8f7942d9
RomanShestakov <RomanShestakov@yahoo.co.uk>
2025-02-18 21:14:52 +0000

    fix typo

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Emqx.MixProject do
     [
       {:gproc, github: "voxoz/gproc"},
       {:ekka, github: "rosemqx/ekka", ref: "v0.7"},
-      {:esockd, github: "rosemqs/esockd", ref: "v5.7.3"},
+      {:esockd, github: "rosemqx/esockd", ref: "v5.7.3"},
       {:gen_rpc, github: "rosemqx/gen_rpc"},
       {:replayq, github: "rosemqx/replayq"},
       {:minirest, github: "rosemqx/minirest", ref: "emqx42"},
